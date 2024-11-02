a031ca65932d327b9ca9fc8d00c47ff88ff1c69c
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2024-11-02 20:46:25 +0530

    first commit

---
+++ pages/_meta.js
@@ -0,0 +1,20 @@
+export default {
+  "index": {
+    "title": "Introduction",
+    "theme": {
+      "layout": "full",
+      "pagination": false,
+    }
+  },
+  "example": "Example",
+  // "about": {
+  //   "title": "About",
+  //   "type": "page"
+  // },
+  "contact": {
+    "title": "Developer Portfolio ↗",
+    "type": "page",
+    "href": "https://omkarbhosale.vercel.app",
+    "newWindow": true
+  }
+}
